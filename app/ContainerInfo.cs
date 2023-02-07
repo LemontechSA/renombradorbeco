e34af98a72fd6ba7bcea5ff8356ca8192c5afb98
John Muñoz <johnmunoz@LMN-0324.local>
2023-02-07 17:46:32 -0300

    feature: micro refactor

---
+++ app/ContainerInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace renombradorbeco.app
+{
+    public class ContainerInfo<T> where T : FileSystemInfo
+    {
+
+        public readonly DirectoryInfo Value;
+
+        public ContainerInfo(DirectoryInfo input)
+        {
+            this.Value = input;
+        }
+
+        public static implicit operator ContainerInfo<T>(string path)
+        {
+            var dirInfo = new DirectoryInfo(path);
+            return new ContainerInfo<T>(dirInfo);
+        }
+    }
+}
